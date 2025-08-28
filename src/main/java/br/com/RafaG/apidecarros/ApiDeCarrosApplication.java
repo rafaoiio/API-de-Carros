a427838d06847d2c92c9d027463a763f5c8ab51c
Rafa <rafag.dias21@gmail.com>
2025-08-27 23:50:04 -0300

    Algumas correções

--- src/main/java/br/com/RafaG/apidecarros/ApiDeCarrosApplication.java
+++ src/main/java/br/com/RafaG/apidecarros/ApiDeCarrosApplication.java
@@ -3,6 +3,7 @@ package br.com.RafaG.apidecarros;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiDeCarrosApplication {
 
---
+++ src/teste/java/br/com/RafaG/apidecarros/ApiDeCarrosApplicationTestes.java
@@ -0,0 +1,13 @@
+package br.com.RafaG.apidecarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeCarrosApplicationTestes {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
